15773adcb188f643e9769c0f62ace8843f6f37b1
Henry-Paul <164559529+Henry-Paul@users.noreply.github.com>
2025-10-31 04:27:25 +0530

    Create tailwind.config.cjs

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#007AFF',
+        whatsapp: '#25D366',
+        phoneorange: '#f7921e'
+      }
+    }
+  },
+  plugins: []
+};
